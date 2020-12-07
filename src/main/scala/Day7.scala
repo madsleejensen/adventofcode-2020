2faa5652de8cac7f4a91db249600c92efd4cf3c3
Mads Lee Jensen <madsleejensen@gmail.com>
2020-12-07 19:53:26 +0100

    day 7 - part 2

--- src/main/scala/Day7.scala
+++ src/main/scala/Day7.scala
@@ -3,6 +3,51 @@ import scala.io.Source
 case class Bag(name: String, quantity: Int)
 
 object Day7 {
+  def partOne(colors: Map[String, List[Bag]]): Int = {
+    def doSearch(list: List[Bag]): Boolean = {
+      list match {
+        case head :: tail =>
+          if (head.name == "shiny gold")
+            return true
+
+          val found = colors.get(head.name) match {
+            case Some(x) => doSearch(x)
+            case None => false
+          }
+
+          found || doSearch(tail)
+        case _ => false
+      }
+    }
+
+    colors.keys.count(p => doSearch(colors(p)))
+  }
+
+  def partTwo(bags: Map[String, List[Bag]]) = {
+    val innerBags = bags("shiny gold")
+
+    def doCount(list: List[Bag]): Int = {
+      list match {
+        case head :: tail => {
+          val curr = bags.get(head.name) match {
+            case Some(x) => {
+              val sub = doCount(x)
+              head.quantity + head.quantity * sub
+            }
+            case None => head.quantity
+          }
+
+          println(s"$head -> $curr")
+
+          curr + doCount(tail)
+        }
+        case Nil => 0
+      }
+    }
+
+    doCount(innerBags)
+  }
+
   def main(args: Array[String]): Unit = {
     val lines = Source.fromResource("day7/input.txt").getLines
     val bagPattern = "^([0-9]+) (.+?) bag.*".r
@@ -23,22 +68,11 @@ object Day7 {
         }
     }.toMap
 
-    def doSearch(list: List[Bag]): Boolean = {
-      list match {
-        case head :: tail =>
-          if (head.name == "shiny gold")
-            return true
+    val partOneResult = partOne(colors)
+    println(s"part-one: $partOneResult bags")
 
-          val found = colors.get(head.name) match {
-            case Some(x) => doSearch(x)
-            case None => false
-          }
+    val partTwoResult = partTwo(colors)
+    println(s"part-two: $partTwoResult bags")
 
-          found || doSearch(tail)
-        case _ => false
-      }
-    }
-
-    println(colors.keys.count(p => doSearch(colors(p))))
   }
 }
